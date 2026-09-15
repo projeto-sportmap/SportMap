1800155f55343792cec21ed06ace635614f3cc84
Fernanda-coder888 <fernanda.lrbaptista@senacsp.edu.br>
2026-09-14 21:33:50 -0300

    comunidadeOK

---
+++ js/btnModalComunidade.js
@@ -0,0 +1,28 @@
+function abrirModal() {
+
+    fetch("html/tela-criar-publicacao.html")
+        .then(resposta => {
+
+            if (!resposta.ok) {
+                throw new Error("Erro ao carregar o arquivo");
+            }
+
+            return resposta.text();
+        })
+        .then(conteudo => {
+
+            document.getElementById("conteudoModal").innerHTML = conteudo;
+
+            document.getElementById("modal").style.display = "flex";
+        })
+        .catch(erro => {
+            console.error(erro);
+            document.getElementById("conteudoModal").innerHTML =
+                "<p>Erro ao carregar a tela.</p>";
+        });
+}
+
+
+function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+}
